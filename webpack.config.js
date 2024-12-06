61352d323833644563e4531eadd17f1936822a77
Sean Dunn <60554213+spdunn@users.noreply.github.com>
2024-12-05 19:14:03 -0500

    Updated webpack config and babel config

--- webpack.config.js
+++ webpack.config.js
@@ -2,29 +2,37 @@ const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  entry: './index.tsx', // Your main React file
-//   output: {
-//     path: path.resolve(__dirname, 'dist'),
-//     filename: 'bundle.js'
-//   },
-//   module: {
-//     rules: [
-//       {
-//         test: /\.js$/,
-//         exclude: /node_modules/,
-//         use: {
-//           loader: 'babel-loader',
-//           options: {
-//             presets: ['@babel/preset-env', '@babel/preset-react']
-//           }
-//         }
-//       }
-//     ]
-//   },
+  entry: './src/index.tsx', // Your main React file
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript']
+          }
+        }
+      },
+      {
+        test: /\.css$/,  // Add this rule
+        use: ['style-loader', 'css-loader'],
+      },
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  },
   plugins: [
     new CopyPlugin({
       patterns: [
-        { from: 'src/util', to: 'dist/util' } // Copy everything from 'public' to 'dist'
+        { from: 'src/util', to: 'dist/util' }, // Copy everything from 'src/util' to 'dist'
+        { from: 'src/components', to: 'dist/'}
       ]
     })
   ]
